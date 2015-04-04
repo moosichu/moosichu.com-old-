903726b4db32395d1f38a574654dec988db0ba24
Tom Read-Cutting <tr395@cam.ac.uk>
2015-04-04 18:51:06 +0100

    Updated about slightly

--- MyWebsite/views/site/about.php
+++ MyWebsite/views/site/about.php
@@ -6,11 +6,14 @@ $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+<h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
+	<p>
+		Hi, my name is Tom Read-Cutting. I am currently an undergraduate computer science student at the University of Cambridge.
+		This website has been written by me in PHP with the help of the <a href="http://www.yiiframework.com/">Yii Framework</a>, you 
+		can find all the sourcecode on <a href="https://github.com/Moosichu/moosichu.com">GitHub</a>.
+		
+		
+	</p>
 
-    <code><?= __FILE__ ?></code>
 </div>
